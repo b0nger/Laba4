95730f962aeca5abe617b1f22d24f0b6b67fb2ff
b0nger <61666891+b0nger@users.noreply.github.com>
2021-06-06 21:15:36 +0300

    Add files via upload

---
+++ Laba4/Laba4/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Laba4
+{
+  public partial class App : Application
+  {
+  }
+}
---
+++ Laba4/Laba4/ProcessUnit.cs
@@ -0,0 +1,17 @@
+﻿namespace Laba4
+{
+  class ProcessUnit
+  {
+    public Process Process { get; set; }
+    public ProcessUnit Next { get; set; }
+    public ProcessUnit(Process processs)
+    {
+      Process = processs;
+    }
+
+    public ProcessUnit()
+    {
+
+    }
+  }
+}
